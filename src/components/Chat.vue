ca8f387950e707723d199e79884a696cdaa2603d
Mohit Chandana <chandanamohit@gmail.com>
2020-05-01 02:52:27 +0530

    Create Chat Window Component

---
+++ src/components/Chat.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="chat container">
+    <h2 class="center teal-text">Mohit Chat</h2>
+    <div class="card">
+      <div class="card-content">
+        <ul class="messeges">
+          <li>
+            <span class="teal-text">Name</span>
+            <span class="grey-text text-darken-3">message</span>
+            <span class="grey-text time">time</span>
+          </li>
+        </ul>
+      </div>
+      <div class="card-action">
+        <NewMessage />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NewMessage from '@/components/NewMessage'
+export default {
+  name: 'Chat',
+  props: ['name'],
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style>
+.chat h2{
+  font-size: 2.6em;
+  margin-bottom: 40px;
+}
+.chat span{
+  font-size: 1.4em;
+}
+.chat .time{
+  display: block;
+  font-size: 1.2em;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Chat from '@/components/Chat'
 
 Vue.use(VueRouter)
 
@@ -17,6 +18,19 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/chat',
+    name: 'Chat',
+    component: Chat,
+    props: true,
+    beforeEnter: (to, from ,next) => {
+      if(to.params.name){
+        next()
+      } else {
+        next({ name: 'Home' })
+      }
+    }
   }
 ]
 
